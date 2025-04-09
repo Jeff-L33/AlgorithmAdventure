7468aa09659d825113e8cb738948da2118e6e0f8
The Happy Monkey <126705970+Jeff-L33@users.noreply.github.com>
2025-04-09 09:10:13 -0600

    Hi
    Bye

--- Inventory.hpp
+++ Inventory.hpp
@@ -1,6 +1,6 @@
 #ifndef INVENTORY
 #define INVENTORY
-
+#include "Item.hpp"
 #include <vector>
 
 class Inventory
@@ -8,5 +8,12 @@ class Inventory
 private:
 	std::vector<Item> slots;
 
+public:
+	Inventory();
+	void addItem(const Item& item);
+	void removeItem(const std::string& itemName);
+	Item getItem(int index) const;
+	int getSize() const;
+	void displayItems() const;
 };
 #endif // !INVENTORY
